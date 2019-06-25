d2d69f326ee48cd2412855c1236d88f443f456e9
Ubuntu <ubuntu@ip-172-31-81-222.ec2.internal>
2019-06-25 14:42:42 +0000

    1)add time delay to the starting and between each level. 2)add check mark in every success level. 3)fix the effect of generating the pattern and clicking

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,12 @@
 var patern = [];
      async function generatePatern(){
-        document.getElementById("start").disabled = true;
+         $("#start").addClass("disabled");
+         $("#failed").addClass("disabled");
+        await sleep(500);
+        $("#failed").removeClass("disabled");
+        $("#start").removeClass("disabled");
+        $("#start").addClass("hide");
+        $("#playing").removeClass("hide");
         var i = 0;
         var j = 0;
         var paternlength = patern.length + 1;
@@ -8,31 +14,37 @@ var patern = [];
            for (j ; j< paternlength; j++){
                var x = Math.floor((Math.random() * 4) + 1);
                patern.push(x);
+               $(".level-num").text(paternlength);
                  // document.getElementById("demo").innerHTML = "[" + patern + "]";
                   if (patern.length == paternlength){
-                       for (var l = 0 ; l < 4; l++){
+                      
+                       for (var l = 0 ; l < 100; l++){
                            if(patern[l] == 1){
+                               $(".active").removeClass("active");
                                $(".blue-quarter").addClass("active");
                             //   removeActiveClass(3000);
-                            setTimeout(removeActiveClass, 1000);
-                            await sleep(2000);
+                            setTimeout(removeActiveClass, 500);
+                            await sleep(1000);
                             
                            }
                            else if (patern[l] == 2){
+                               $(".active").removeClass("active");
                                    $(".red-quarter").addClass("active");
-                                   setTimeout(removeActiveClass, 1000);
-                                   await sleep(2000);
+                                   setTimeout(removeActiveClass, 500);
+                                   await sleep(1000);
                                }
                             else  if (patern[l] == 3){
+                                $(".active").removeClass("active");
                                    $(".green-quarter").addClass("active");
-                                   setTimeout(removeActiveClass, 1000);
-                                   await sleep(2000);
+                                   setTimeout(removeActiveClass, 500);
+                                   await sleep(1000);
                                }
                             else {
                             if (patern[l] == 4){
+                                $(".active").removeClass("active");
                                    $(".yellow-quarter").addClass("active");
-                                   setTimeout(removeActiveClass, 1000);
-                                   await sleep(2000);
+                                   setTimeout(removeActiveClass, 500);
+                                   await sleep(1000);
                                }
                            
                        }
@@ -55,28 +67,28 @@ var patern = [];
         var blueButton = document.getElementById("blueButtonId");
         var orangeButton = document.getElementById("orangeButtonId");
         if (button != null  && button.id == redButton.id){
-            alert ("Red is clicked");
+           // alert ("Red is clicked");
             inputPatern.push(2);
             checkPaternsLenght(inputPatern, generatedPatern);
             return inputPatern;
             } 
             else 
             if (button != null  && button.id == greenButton.id){
-                alert ("green is clicked");
+                //alert ("green is clicked");
                 inputPatern.push(3);
                 checkPaternsLenght(inputPatern, generatedPatern);
                 return inputPatern;
             }
             else 
             if (button != null  && button.id == blueButton.id){
-                alert ("Blue is clicked");
+               // alert ("Blue is clicked");
                 inputPatern.push(1);
                 checkPaternsLenght(inputPatern, generatedPatern);
                 return inputPatern;
             }
             else 
             if (button != null  && button.id == orangeButton.id){
-                alert ("Orange is clicked");
+               // alert ("Orange is clicked");
                 inputPatern.push(4);
                 checkPaternsLenght(inputPatern, generatedPatern);
                 return inputPatern;
@@ -87,22 +99,30 @@ var patern = [];
             }
             
             
-            function checkPaternsLenght(userPatern,generatedPatern ){
+          async  function checkPaternsLenght(userPatern,generatedPatern ){
                 var userPaternLenght = userPatern.length;
                 var generatedPaternLength = generatedPatern.length;
                 
               if (userPaternLenght == generatedPaternLength) {
-                  alert ("length is equals");
-                  alert(generatedPatern.toString());
-                  alert(userPatern.toString());
+                  //alert ("length is equals");
+                 // alert(generatedPatern.toString());
+                 // alert(userPatern.toString());
                      if (generatedPatern.toString() == userPatern.toString()){
-                         alert("Success");
+                         //alert("Success");
+                        // $(".level").append("<i class='fas fa-check'></i>");
+                        $("#result-icon").removeClass("hide");
+                        $(".level").addClass("hide");await sleep(1000);
+                        $("#result-icon").addClass("hide");
+                        $(".level").removeClass("hide");
+                        // $(this).children("i").remove();
                          inputPatern = [];
                          generatedPatern = [];
                          generatePatern();
                      }
                      else{
-                         alert("Not");
+                         $("#playing").addClass("hide");
+                         $("#failed").removeClass("hide");
+                        // alert("Not");
                           //document.getElementById("demo").innerHTML = "";
                           inputPatern = [];
                           generatedPatern = [];
@@ -119,9 +139,12 @@ var patern = [];
                           }
                           
                           else {
-                              alert("wrong input");
+                               $("#playing").addClass("hide");
+                         $("#failed").removeClass("hide");
+                              //alert("wrong input");
                               generatedPatern = [];
                               inputPatern = [];
+                              patern = [];
                               // document.getElementById("demo").innerHTML = "";
                                //document.getElementById("start").disabled = false;
                               
@@ -138,3 +161,8 @@ var patern = [];
             function sleep(ms) {
             return new Promise(resolve => setTimeout(resolve, ms));
             }
+            
+         
+
+            
+            
